56afc7c6248dea34cffbd6c2c318e01d20465189
vitgavrilchenko <vitgavrilchenko@gmail.com>
2024-08-26 22:13:38 +0300

    one to one crete find delete

---
+++ src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,37 @@
+package com.luv2code.cruddemo.dao;
+
+import com.luv2code.cruddemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        entityManager.persist(theInstructor);
+
+    }
+
+    @Override
+    public Instructor findInstructorById(int theId) {
+        return entityManager.find(Instructor.class, theId);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int theId) {
+        Instructor theInstructor = entityManager.find(Instructor.class, theId);
+        entityManager.remove(theInstructor);
+    }
+}
