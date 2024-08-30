399f192850a3cfb6fc5bcc18c27b46c057244ac7
vitgavrilchenko <vitgavrilchenko@gmail.com>
2024-08-30 10:50:17 +0300

    one to many join fetch when lazy

--- src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -30,8 +30,23 @@ public class CruddemoApplication {
 
                 //findInstructorWithCourses(appDAO);
 
-                findCoursesForInstructor(appDAO);
+                //findCoursesForInstructor(appDAO);
 
+                findInstructorWithCourcesJoinFetch(appDAO);
+
+    }
+
+    private void findInstructorWithCourcesJoinFetch(AppDAO appDAO) {
+
+        int theId = 1;
+
+        System.out.println("finding instructor id: " + theId);
+        Instructor tempInstructor = appDAO.findInstructorByIdJoinFetch(theId);
+
+        System.out.println("tempInstructor: " + tempInstructor);
+        System.out.println("the associated courses: " + tempInstructor.getCourses());
+
+        System.out.println("Done!");
     }
 
     private void findCoursesForInstructor(AppDAO appDAO) {
--- src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
+++ src/main/java/com/luv2code/cruddemo/dao/AppDAOImpl.java
@@ -64,4 +64,16 @@ public class AppDAOImpl implements AppDAO {
 
         return courses;
     }
+
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int theId) {
+        TypedQuery<Instructor> query = entityManager.createQuery("select i from Instructor i " +
+                "JOIN FETCH i.courses " +
+                "JOIN FETCH i.instructorDetail where i.id = :data", Instructor.class);
+        query.setParameter("data", theId);
+
+        Instructor instructor = query.getSingleResult();
+
+        return instructor;
+    }
 }
